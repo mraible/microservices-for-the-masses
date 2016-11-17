6fbcc8ea22c00c3857b9b00067d248f8c54f3018
Matt Raible <matt@raibledesigns.com>
2016-11-17 13:45:29 -0700

    Spring Boot + JPA + REST + Stormpath demo

--- demos/spring-boot/src/main/java/com/example/DemoApplication.java
+++ demos/spring-boot/src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,70 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@Entity
+class Blog {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+@RepositoryRestResource
+interface BlogRepository extends PagingAndSortingRepository<Blog, Long> {
+}
+
+@Component
+class BlogCommandLineRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println(repository.findAll());
+    }
+
+    @Autowired
+    private BlogRepository repository;
 }
