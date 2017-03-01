fefe73da748989bc888a45653a6f7116c8710404
Matt Raible <matt@raibledesigns.com>
2017-02-28 21:22:36 -0700

    Updated for latest releases of Spring Boot and Stormpath

--- demos/spring-boot/src/main/java/com/example/DemoApplication.java
+++ demos/spring-boot/src/main/java/com/example/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -59,12 +58,13 @@ interface BlogRepository extends PagingAndSortingRepository<Blog, Long> {
 
 @Component
 class BlogCommandLineRunner implements CommandLineRunner {
+    private BlogRepository repository;
 
+    public BlogCommandLineRunner(BlogRepository repository) {
+        this.repository = repository;
+    }
     @Override
     public void run(String... strings) throws Exception {
         System.out.println(repository.findAll());
     }
-
-    @Autowired
-    private BlogRepository repository;
 }
